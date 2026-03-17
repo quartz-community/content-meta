d56cb6297a88935075746c5082f2f26d63e3e15b
saberzero1 <github@emilebangma.com>
2026-03-17 19:31:51 +0100

    fix: bundle non-shared deps into dist to prevent runtime resolution failures
    Move non-allowlisted packages from dependencies/peerDependencies to
    devDependencies so tsup bundles them into dist/ instead of leaving
    them as external imports that may not resolve at runtime.

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,165 @@
-import readingTime from 'reading-time';
 import { jsx } from 'preact/jsx-runtime';
 
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
+  get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
+}) : x)(function(x) {
+  if (typeof require !== "undefined") return require.apply(this, arguments);
+  throw Error('Dynamic require of "' + x + '" is not supported');
+});
+var __commonJS = (cb, mod) => function __require2() {
+  return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  __defProp(target, "default", { value: mod, enumerable: true }) ,
+  mod
+));
+
+// node_modules/reading-time/lib/reading-time.js
+var require_reading_time = __commonJS({
+  "node_modules/reading-time/lib/reading-time.js"(exports$1, module) {
+    function codeIsInRanges(number, arrayOfRanges) {
+      return arrayOfRanges.some(
+        ([lowerBound, upperBound]) => lowerBound <= number && number <= upperBound
+      );
+    }
+    function isCJK(c) {
+      if ("string" !== typeof c) {
+        return false;
+      }
+      const charCode = c.charCodeAt(0);
+      return codeIsInRanges(
+        charCode,
+        [
+          // Hiragana (Katakana not included on purpose,
+          // context: https://github.com/ngryman/reading-time/pull/35#issuecomment-853364526)
+          // If you think Katakana should be included and have solid reasons, improvement is welcomed
+          [12352, 12447],
+          // CJK Unified ideographs
+          [19968, 40959],
+          // Hangul
+          [44032, 55203],
+          // CJK extensions
+          [131072, 191456]
+        ]
+      );
+    }
+    function isAnsiWordBound(c) {
+      return " \n\r	".includes(c);
+    }
+    function isPunctuation(c) {
+      if ("string" !== typeof c) {
+        return false;
+      }
+      const charCode = c.charCodeAt(0);
+      return codeIsInRanges(
+        charCode,
+        [
+          [33, 47],
+          [58, 64],
+          [91, 96],
+          [123, 126],
+          // CJK Symbols and Punctuation
+          [12288, 12351],
+          // Full-width ASCII punctuation variants
+          [65280, 65519]
+        ]
+      );
+    }
+    function readingTime2(text, options = {}) {
+      let words = 0, start = 0, end = text.length - 1;
+      const wordsPerMinute = options.wordsPerMinute || 200;
+      const isWordBound = options.wordBound || isAnsiWordBound;
+      while (isWordBound(text[start])) start++;
+      while (isWordBound(text[end])) end--;
+      const normalizedText = `${text}
+`;
+      for (let i = start; i <= end; i++) {
+        if (isCJK(normalizedText[i]) || !isWordBound(normalizedText[i]) && (isWordBound(normalizedText[i + 1]) || isCJK(normalizedText[i + 1]))) {
+          words++;
+        }
+        if (isCJK(normalizedText[i])) {
+          while (i <= end && (isPunctuation(normalizedText[i + 1]) || isWordBound(normalizedText[i + 1]))) {
+            i++;
+          }
+        }
+      }
+      const minutes = words / wordsPerMinute;
+      const time = Math.round(minutes * 60 * 1e3);
+      const displayed = Math.ceil(minutes.toFixed(2));
+      return {
+        text: displayed + " min read",
+        minutes,
+        time,
+        words
+      };
+    }
+    module.exports = readingTime2;
+  }
+});
+
+// node_modules/reading-time/lib/stream.js
+var require_stream = __commonJS({
+  "node_modules/reading-time/lib/stream.js"(exports$1, module) {
+    var readingTime2 = require_reading_time();
+    var Transform = __require("stream").Transform;
+    var util = __require("util");
+    function ReadingTimeStream(options) {
+      if (!(this instanceof ReadingTimeStream)) {
+        return new ReadingTimeStream(options);
+      }
+      Transform.call(this, { objectMode: true });
+      this.options = options || {};
+      this.stats = {
+        minutes: 0,
+        time: 0,
+        words: 0
+      };
+    }
+    util.inherits(ReadingTimeStream, Transform);
+    ReadingTimeStream.prototype._transform = function(chunk, encoding, callback) {
+      const stats = readingTime2(chunk.toString(encoding), this.options);
+      this.stats.minutes += stats.minutes;
+      this.stats.time += stats.time;
+      this.stats.words += stats.words;
+      callback();
+    };
+    ReadingTimeStream.prototype._flush = function(callback) {
+      this.stats.text = Math.ceil(this.stats.minutes.toFixed(2)) + " min read";
+      this.push(this.stats);
+      callback();
+    };
+    module.exports = ReadingTimeStream;
+  }
+});
+
+// node_modules/reading-time/index.js
+var require_reading_time2 = __commonJS({
+  "node_modules/reading-time/index.js"(exports$1, module) {
+    module.exports.default = module.exports = require_reading_time();
+    module.exports.readingTimeStream = require_stream();
+  }
+});
+
 // src/components/ContentMeta.tsx
+var import_reading_time = __toESM(require_reading_time2());
 
 // src/util/lang.ts
 function classNames(...classes) {
@@ -66,7 +224,7 @@ var ContentMeta_default = ((opts) => {
         segments.push(/* @__PURE__ */ jsx(DateComponent, { date: getDate(cfg, fileData), locale }));
       }
       if (options.showReadingTime) {
-        const { minutes, words: _words } = readingTime(text);
+        const { minutes, words: _words } = (0, import_reading_time.default)(text);
         const locale = cfg.locale || "en-US";
         const i18nData = i18n(locale);
         const displayedTime = i18nData.components.contentMeta.readingTime({
@@ -82,6 +240,16 @@ var ContentMeta_default = ((opts) => {
   ContentMetadata.css = contentMeta_default;
   return ContentMetadata;
 });
+/*! Bundled license information:
+
+reading-time/lib/reading-time.js:
+reading-time/lib/stream.js:
+  (*!
+   * reading-time
+   * Copyright (c) Nicolas Gryman <ngryman@gmail.com>
+   * MIT Licensed
+   *)
+*/
 
 export { ContentMeta_default as ContentMeta };
 //# sourceMappingURL=index.js.map
